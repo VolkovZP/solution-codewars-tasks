c36609c32cf397028a91d66dfda47bcc976a23ea
Volkov <twen1200712@gmail.com>
2021-09-05 18:39:35 +0300

    ROT13

--- index.js
+++ index.js
@@ -1,42 +1,22 @@
 'use strict';
 /*
-# Highest Scoring Word
+# ROT13
 
- Given a string of words, you need to find the highest scoring word.
- Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
- You need to return the highest scoring word as a string.
- If two words score the same, return the word that appears earliest in the original string.
- All letters will be lowercase and all inputs will be valid.
+ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
 */
-function high(x) {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    const objAlphabet = {};
-    const arrNumber = []
-    const arrInlineStr = [];
-    let accamulator = 0;
-    alphabet.split('').forEach((item, index) => {
-        objAlphabet[item] = ++index
-    })
-    const arrayIsArray = x.split(' ');
-    for (let i = 0; i < arrayIsArray.length; i++) {
-        arrInlineStr.push([...arrayIsArray[i]])
-    }
-    for (let i = 0; i < arrInlineStr.length; i++) {
-        for (let k = 0; k < arrInlineStr[i].length; k++) {
-            accamulator += objAlphabet[arrInlineStr[i][k]]
-        }
-        arrNumber.push(accamulator)
-        accamulator = 0;
-    }
-    let indexValue = 0
-    let maxValue = arrNumber[0];
-    for (let i = 0; i < arrNumber.length; i++) {
-        if (arrNumber[i] > maxValue) {
-            maxValue = arrNumber[i];
-            indexValue = i
-        }
-    }
-    return arrayIsArray[indexValue]
-}
 
-console.log(high('hello world'))
+function rot13(string) {
+    const input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+    let res = ''
+    for (let i = 0; i < string.length; i++) {
+        let index = input.indexOf(string[i])
+        if (index !== -1) {
+            res += output[index]
+        } else res += string[i]
+    }
+    return res;
+}
+console.log(rot13('hello'))
