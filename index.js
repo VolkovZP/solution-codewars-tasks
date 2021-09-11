5cda3a99a5d68bede7f7176744b78065edbb9ee6
Volkov <mark.volkovgit@gmail.com>
2021-09-11 15:22:06 +0300

    Tic-Tac-Toe Checker

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 'use strict';
 /*
-# Moving Zeros To The End
+# Tic-Tac-Toe Checker
 
-Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+If we were to set up a Tic-Tac-Toe game, we would want to know whether the board's current state is solved, wouldn't we? Our goal is to create a function that will check that for us!
+
+Assume that the board comes in the form of a 3x3 array, where the value is 0 if a spot is empty, 1 if it is an "X", or 2 if it is an "O", like so:
+
+[[0, 0, 1],
+ [0, 1, 2],
+ [2, 1, 0]]
+We want our function to return:
+
+-1 if the board is not yet finished AND no one has won yet (there are empty spots),
+1 if "X" won,
+2 if "O" won,
+0 if it's a cat's game (i.e. a draw).
+You may assume that the board passed in is valid in the context of a game of Tic-Tac-Toe.
 */
 
-//returns[false,1,1,2,1,3,"a",0,0]
 
-let moveZeros = function (arr) {
-    let firstSubArr = arr.filter(item => item !== 0);
-    let secondSubArr = arr.filter(item => item === 0);
-    return [...firstSubArr, ...secondSubArr]
+function isSolved(board) {
+    for (let i = 0; i < board.length; i++) {
+        if ((board[i].filter(a => a === 1)).length === 3) return 1
+        if ((board[i].filter(a => a === 2)).length === 3) return 2
+        for (let j = 1; j <= 2; j++) {
+            if (board[0][i] === j && board[1][i] === j && board[2][i] === j) return j
+        }
+        //
+        if (board[0][0] === 1 && board[1][1] === 1 && board[2][2] === 1) return 1
+        if (board[0][2] === 1 && board[1][1] === 1 && board[2][0] === 1) return 1
+        if (board[0][2] === 2 && board[1][1] === 2 && board[2][0] === 2) return 2
+        if (board[0][2] === 2 && board[1][1] === 2 && board[2][0] === 2) return 2
+    }
+    for (let i = 0; i < board.length; i++) {
+        if ((board[i].filter(a => a === 0).length) > 0) return -1
+    }
+    return 0
 }
-console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]));
