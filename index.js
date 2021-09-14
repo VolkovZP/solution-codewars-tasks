f65846375e33fbe10b84451c030419022b00d580
Volkov <mark.volkovgit@gmail.com>
2021-09-14 18:16:49 +0300

    Polybius square cipher - encode

--- index.js
+++ index.js
@@ -1,24 +1,62 @@
 'use strict';
 /*
-# Detect Pangram
+# Polybius square cipher - encode
 
-A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+Implement the Polybius square cipher.
 
-Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+Replace every letter with a two digit number. The first digit is the row number, and the second digit is the column number of following square. Letters 'I' and 'J' are both 24 in this cipher:
+
+    1	2	3	4	5
+1	A	B	C	D	E
+2	F	G	H	I/J	K
+3	L	M	N	O	P
+4	Q	R	S	T	U
+5	V	W	X	Y	Z
+
+Input will be valid (only spaces and uppercase letters from A to Z), so no need to validate them.
+
 */
 
-function isPangram(string) {
-    const obj = {
-        valid: ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z', 'a', 'e', 'i', 'o', 'u', 'y'].sort(),
-        notValid: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '.', ','],
-    }
-    let result = []
-    string = Array.from(new Set(string.toLowerCase().split('').sort()));
-    for (let item of string) {
-        if (!obj['notValid'].includes(item)) result.push(item);
-    }
-    if (result.join('') === obj['valid'].join('')) return true;
-    return false
+const obj2 = {
+    A: 11,
+    B: 12,
+    C: 13,
+    D: 14,
+    E: 15,
+    F: 21,
+    G: 22,
+    H: 23,
+    I: 24,
+    J: 24,
+    K: 25,
+    L: 31,
+    M: 32,
+    N: 33,
+    O: 34,
+    P: 35,
+    Q: 41,
+    R: 42,
+    S: 43,
+    T: 44,
+    U: 45,
+    V: 51,
+    W: 52,
+    X: 53,
+    Y: 54,
+    Z: 55,
+    ' ': ' ',
 }
-console.log(isPangram("The quick brown fox jumps over the lazy dog"))
 
+function polybius(str) {
+    let f = '';
+    console.log(str)
+    for (let i = 0; i < str.length; i++) {
+        let ind = str.indexOf(str[i], i);
+        if (ind !== -1) {
+            f += obj2[str[i]];
+            i = ind
+        }
+    }
+    return f
+}
+console.log(polybius('POLYBIUS SQUARE CIPHER'))
