178764f45d9aba6c95ae7910a91ad4f30f1d571d
Volkov <mark.volkovgit@gmail.com>
2021-09-18 10:12:24 +0300

    Uncollapse Digits

--- index.js
+++ index.js
@@ -1,62 +1,28 @@
 'use strict';
-/*
-# Polybius square cipher - encode
+/* # Uncollapse Digits
 
-Implement the Polybius square cipher.
+Task
+You will be given a string of English digits "stuck" together, like this:
 
-Replace every letter with a two digit number. The first digit is the row number, and the second digit is the column number of following square. Letters 'I' and 'J' are both 24 in this cipher:
+"zeronineoneoneeighttwoseventhreesixfourtwofive"
 
-    1	2	3	4	5
-1	A	B	C	D	E
-2	F	G	H	I/J	K
-3	L	M	N	O	P
-4	Q	R	S	T	U
-5	V	W	X	Y	Z
+Your task is to split the string into separate digits:
 
-Input will be valid (only spaces and uppercase letters from A to Z), so no need to validate them.
+"zero nine one one eight two seven three six four two five"
 
 */
 
-const obj2 = {
-    A: 11,
-    B: 12,
-    C: 13,
-    D: 14,
-    E: 15,
-    F: 21,
-    G: 22,
-    H: 23,
-    I: 24,
-    J: 24,
-    K: 25,
-    L: 31,
-    M: 32,
-    N: 33,
-    O: 34,
-    P: 35,
-    Q: 41,
-    R: 42,
-    S: 43,
-    T: 44,
-    U: 45,
-    V: 51,
-    W: 52,
-    X: 53,
-    Y: 54,
-    Z: 55,
-    ' ': ' ',
-}
+const numberArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
 
-function polybius(str) {
-    let f = '';
-    console.log(str)
-    for (let i = 0; i < str.length; i++) {
-        let ind = str.indexOf(str[i], i);
-        if (ind !== -1) {
-            f += obj2[str[i]];
-            i = ind
+function uncollapse(digits, str = '', getDigits = []) {
+    for (let i = 0; i < digits.length; i++) {
+        str += digits[i];
+        if (numberArray.includes(str)) {
+            getDigits.push(str);
+            str = '';
         }
     }
-    return f
+    return getDigits.join(' ')
 }
-console.log(polybius('POLYBIUS SQUARE CIPHER'))
+
+console.log(uncollapse('foursixeighttwofive'))
