efe51be6c5466854357963b624918a77f4c82efc
Volkov <mark.volkovgit@gmail.com>
2021-11-15 21:09:52 +0200

    Convert PascalCase string into snake_case

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 'use strict';
-/* # Mexican Wave
+/* # Convert PascalCase string into snake_case
 
-The wave (known as the Mexican wave in the English-speaking world outside North America) is an example of metachronal rhythm achieved in a packed stadium when successive groups of spectators briefly stand, yell, and raise their arms. Immediately upon stretching to full height, the spectator returns to the usual seated position.
+Complete the function/method so that it takes a PascalCase string and returns the string in snake_case notation. Lowercase characters can be numbers. If the method gets a number as input, it should return a string.
 
-The result is a wave of standing spectators that travels through the crowd, even though individual spectators never move away from their seats. In many large arenas the crowd is seated in a contiguous circuit all the way around the sport field, and so the wave is able to travel continuously around the arena; in discontiguous seating arrangements, the wave can instead reflect back and forth through the crowd. When the gap in seating is narrow, the wave can sometimes pass through it. Usually only one wave crest will be present at any given time in an arena, although simultaneous, counter-rotating waves have been produced. (Source Wikipedia)
-Task
-In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.
-Rules
- 1.  The input string will always be lower case but maybe empty.
 
- 2.  If the character in the string is whitespace then pass over it as if it was an empty seat
-Example
-wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+"TestController"  -->  "test_controller"
+"MoviesAndBooks"  -->  "movies_and_books"
+"App7Test"        -->  "app7_test"
+1                 -->  "1"
+
 
 */
-
-
-const wave = str =>
-    [...str].map((s, i) =>
-        str.slice(0, i) + s.toUpperCase() + str.slice(i + 1)
-    ).filter(x => x != str);
+function toUnderscore(string) {
+    let arr = [];
+    string = string.toString();
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === string[i].toUpperCase() && !+string[i]) arr.push('_');
+        arr.push(string[i].toLowerCase());
+    };
+    let index = arr.indexOf('_');
+    if (index > -1) {
+        arr.splice(index, 1);
+    };
+    return arr.join('')
+};
+console.log(toUnderscore('TestController'))
+console.log(toUnderscore("MoviesAndBooks"))
+console.log(toUnderscore("App7Test"))
+console.log(toUnderscore(1))
